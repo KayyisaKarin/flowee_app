cd086f3f5e3b1a8736fc31a45d238f7b51d21279
Kayyisa Karin <kaysemail20@gmail.com>
2026-08-31 10:40:14 +0700

    Add Theme

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,10 +5,11 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:flowee_app/wa.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flowee_app/main.dart';
+// import 'package:flowee_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
