49699a6d01491727dabc37a8cf3a8587c5861723
Kayyisa Karin <kaysemail20@gmail.com>
2026-09-01 08:55:14 +0700

    Adding Circle Icon Button

---
+++ lib/widgets/sheet_drag_handle.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+// Garis Kecil abu di bagian atas atau sebuah "Sheet UI" sbg tanda, bahwa screen/sheet
+class SheetDragHandle extends StatelessWidget {
+  const SheetDragHandle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
