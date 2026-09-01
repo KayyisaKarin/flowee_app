16aa36dda9577ea459694443fd61e76904d4669f
Kayyisa Karin <kaysemail20@gmail.com>
2026-09-01 08:22:47 +0700

    Adding error handling for network image

--- lib/widgets/flower_image.dart
+++ lib/widgets/flower_image.dart
@@ -16,6 +16,52 @@ class FlowerNetworkImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Image.network(
+      imageUrl,
+      fit: fit,
+      width: double.infinity,
+      height: double.infinity,
+      // Loading flutter
+      loadingBuilder: (context, child, progress) {
+        if (progress == null) return child;
+        return _Placeholder(
+          color: fallbackColor,
+          child: SizedBox(
+            width: 22,
+            height: 22,
+            child: CircularProgressIndicator(
+              strokeWidth: 2.2,
+              color: fallbackColor,
+              value: progress.expectedTotalBytes != null ? progress.cumulativeBytesLoaded / progress. expectedTotalBytes! : null,
+              // Flutter tau ukuran total file, maka akan menghitung proses download gambar
+              // x => Kembalikan Null
+            ),
+          ),
+        );
+      },
+      // Error Builder -> Backup if the process fails.
+      errorBuilder: (context, error, stackTrace) {
+        return _Placeholder(
+          color: fallbackColor, 
+          child: Icon(fallbackIcon, size: 48, color: fallbackColor)
+          );
+      },
+    );
+  }
+}
+
+class _Placeholder extends StatelessWidget {
+  const _Placeholder({required this.color, required this.child});
+
+  final Color color;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: color.withValues(alpha: 0.18),
+      alignment: Alignment.center,
+      child: child,
+    );
   }
 }
