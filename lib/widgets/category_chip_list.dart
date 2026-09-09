ce8d75c3ac71210efe657c1a3b90f02e447652ee
Kayyisa Karin <kaysemail20@gmail.com>
2026-09-09 09:12:53 +0700

    Polishing Search Field widget

---
+++ lib/widgets/category_chip_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CategoryChipList extends StatelessWidget {
+  const CategoryChipList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
